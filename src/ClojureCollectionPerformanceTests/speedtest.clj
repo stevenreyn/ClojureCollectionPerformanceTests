2fdf59de29833f1db23ad6179cbb2fcc5b6be3c6
Steven Reynolds <steven.lamar.reynolds@gmail.com>
2013-08-18 11:42:02 -0500

    Working on compile errors. Updated cautions at top of file.

--- src/ClojureCollectionPerformanceTests/speedtest.clj
+++ src/ClojureCollectionPerformanceTests/speedtest.clj
@@ -1,10 +1,14 @@
-(ns net.slreynolds.ds.speedtest)
+(ns speedtest)
 
-;;; Caution: does not work yet
+;;; Caution: does not compile and/or work yet
+;;; Caution: even if it does run, the timing is vastly over estimated
+
+(import 'net.slreynolds.ds.IntMapSource)
+(import 'net.slreynolds.ds.SomeValue)
 
 ; Warmup the jit compiler etc in the JVM
 ; before doing any measurements
-(defn warmup[setup dowork]
+(defn warmup[setup dowork N]
   (let [computation 7]
        (dotimes [_ N]
          (let [o (setup)
@@ -25,9 +29,9 @@
           (let [runt (Runtime/getRuntime)
                 before (. runt totalMemory)
                 res (dowork o)
-                (System/gc)
-                (System/gc)
-                (System/gc)
+                _ (System/gc)
+                _ (System/gc)
+                _ (System/gc)
                 after (. runt totalMemory)
                 otherComp (+ computation (. res getIntParam))]
             (Math/max 0 (- after before)))))
@@ -40,14 +44,12 @@
          times []
          computation 7]
     (let [o (setup)
-          before (System/nanotime)
+          before (System/nanoTime)
           res (dowork o)
-          after (System/nanotime)
-          (conj times (- after before))]
+          after (System/nanoTime)]
       (if (zero? ctr)
-        times
-        (recur ((dec ctr) times (+ computation (. res getIntParam))))))))
-      
+        (conj times (- after before))
+        (recur (dec ctr) (conj times (- after before)) (+ computation (. res getIntParam)))))))      
        
 ; Run tests: warmup, measure-space, and then timeit
 ; Print results
@@ -60,7 +62,7 @@
       (print "Times (seconds)")
       (print times))))
         
-; setup implmentation for std clojure hashmap
+; setup implementation for std clojure hashmap
 (defn htsetup[]
   (let [is (range 0 (- IntMapSource/initialSize 1))
         vs (map #(SomeValue. %) is)
@@ -69,7 +71,7 @@
 
 ; dowork implementation for std clojure hashmap
 (defn htdowork[themap]
-  ; Note doing a lot of extra work here!
+  ; Note doing a lot of extra work here inside the timed function!
   ; Making a list ahead of time is very important!!!!
   (let [list-of-vs (list IntMapSource/valuesToInsert)]
     (loop [newmap themap

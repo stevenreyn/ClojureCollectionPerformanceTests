37e6f81124ad1d760eb89bad75101b8159194ecc
Steven Reynolds <steven.lamar.reynolds@gmail.com>
2013-08-18 14:55:02 -0500

    Well it runs now, just the statistics are garbage

--- src/ClojureCollectionPerformanceTests/speedtest.clj
+++ src/ClojureCollectionPerformanceTests/speedtest.clj
@@ -1,15 +1,17 @@
 (ns speedtest)
 
-;;; Caution: does not work yet
-;;; Caution: even if it does run, the timing is vastly over estimated
+;;; Caution: does not work yet; times are wrong, space is wrong.
+;;; Caution: the work in the timed function has lots of overhead
 
 (import 'net.slreynolds.ds.IntMapSource)
 (import 'net.slreynolds.ds.SomeValue)
+(import 'net.slreynolds.ds.Result)
 (import 'scala.Tuple2) ; TODO probably shouldn't leave this
 
 ; Warmup the jit compiler etc in the JVM
 ; before doing any measurements
 (defn warmup[setup dowork N]
+  (println "warming up" N)
   (let [computation 7]
        (dotimes [_ N]
          (let [o (setup)
@@ -21,7 +23,7 @@
 ; Measure space (bytes) consumed by result of
 ; of dowork
 (defn measure-space[setup dowork]
-      
+        (println "measuring space")
         (let [computation 7
               o (setup)]
           (System/gc)
@@ -41,6 +43,7 @@
 ; Do setup and then dowork N times. Record the times taken in dowork.
 ; Return list of times
 (defn timeit[setup dowork N]
+  (println "timeing it")
   (loop [ctr (- N 1)
          times []
          computation 7]
@@ -49,39 +52,47 @@
           res (dowork o)
           after (System/nanoTime)]
       (if (zero? ctr)
-        (conj times (- after before))
-        (recur (dec ctr) (conj times (- after before)) (+ computation (. res getIntParam)))))))      
+        (conj times (* (- after before) 1.E-9))
+        (recur (dec ctr) (conj times (* (- after before) 1.E-9)) (+ computation (. res getIntParam)))))))      
        
 ; Run tests: warmup, measure-space, and then timeit
 ; Print results
 (defn run [setup dowork title]
+  (println "runing" title)
   (let [res (warmup setup dowork 20)]
     (print "res " res)
     (let [space (measure-space setup dowork)
           times (timeit setup dowork 5)]
-      (print title " uses " space " bytes ")
-      (print "Times (seconds)")
-      (print times))))
+      (println title "uses" space "bytes")
+      (println "Times (seconds)")
+      (println times)
+      (println "thanks, all done now"))))
         
-(defn create-some-value[#^Integer i]
+(defn create-some-value[#^Long i]
   (SomeValue. i))
 
 ; setup implementation for std clojure hashmap
 (defn htsetup[]
-  (let [is (range 0 (- (IntMapSource/initialSize) 1))
-        vs (map create-some-value is)
-        themap (hash-map (interleave is vs))]
-    themap))
+  (println "htsetup")
+  (let [initial-is (range 0 (- (IntMapSource/initialSize) 1))
+        themap {}]
+    (loop [newmap themap
+           is initial-is]
+      (if (empty is)
+        newmap
+        (recur (assoc newmap (first is)) (rest is))))))
+
 
 ; dowork implementation for std clojure hashmap
 (defn htdowork[themap]
+  (println "htdowork")
   ; Note doing a lot of extra work here inside the timed function!
   ; Making a list ahead of time is very important!!!!
   (let [initial-vs (list (IntMapSource/valuesToInsert))]
     (loop [newmap themap
            vs initial-vs] 
       (if (empty vs)
-        newmap
+        (Result. newmap 7654321)
         (recur (assoc newmap (first vs)) (rest vs))))))
 
 ; Run the test for std clojure hashmap
